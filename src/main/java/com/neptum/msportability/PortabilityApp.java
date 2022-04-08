65e9255ca4e192aa515aa60d34456a6f1ac7ff9e
Matheus Augusto <matheus_adps@hotmail.com>
2022-04-07 22:31:05 -0300

    Classe principal

---
+++ src/main/java/com/neptum/msportability/PortabilityApp.java
@@ -0,0 +1,13 @@
+package com.neptum.msportability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortabilityApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run( PortabilityApp.class, args );
+    }
+}
